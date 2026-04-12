f9aee0ed6a678e7bfd2ace1ef179a8d0d25244fe
emiliooavila <avilavaladezjorgeemilio1010@gmail.com>
2026-04-12 15:37:34 -0600

    🐛fix: se corrigieron los errores de apis y credenciales, ya se conecta bien a MySQL

--- backend-libreria/index.js
+++ backend-libreria/index.js
@@ -1,19 +1,61 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2/promise');
 
 const app = express();
 const PORT = 3000;
 
 // Middlewares
 app.use(cors());
-app.use(express.json()); // Para poder leer JSON en el body de las peticiones
+app.use(express.json());
 
-// Ruta de prueba
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'libreria_db'
+});
+
 app.get('/', (req, res) => {
     res.send('¡Servidor de la librería funcionando correctamente!');
 });
 
-// Arrancar el servidor
+app.get('/api/productos', async (req, res) => {
+    try {
+        const [rows] = await db.query('SELECT * FROM productos');
+        res.json(rows);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al obtener los productos' });
+        console.error('Este es el error real:', error);
+    }
+});
+
+app.post('/api/productos', async (req, res) => {
+    try {
+        const { nombre, categoria, marca, precio, stock, imagen, descripcion } = req.body;
+        const [result] = await db.query(
+            'INSERT INTO productos (nombre, categoria, marca, precio, stock, imagen, descripcion) VALUES (?, ?, ?, ?, ?, ?, ?)',
+            [nombre, categoria, marca, precio, stock, imagen, descripcion]
+        );
+        res.status(201).json({ id: result.insertId, mensaje: 'Libro agregado al catálogo' });
+    } catch (error) {
+        res.status(500).json({ error: 'Error al guardar el libro' });
+    }
+});
+
+app.post('/api/mensajes', async (req, res) => {
+    try {
+        const { nombre, correo, asunto, mensaje } = req.body;
+        const [result] = await db.query(
+            'INSERT INTO mensajes (nombre, correo, asunto, mensaje) VALUES (?, ?, ?, ?)',
+            [nombre, correo, asunto, mensaje]
+        );
+        res.status(201).json({ id: result.insertId, mensaje: 'Mensaje de contacto guardado' });
+    } catch (error) {
+        res.status(500).json({ error: 'Error al guardar el mensaje' });
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en \x1b[34mhttp://localhost:${PORT}\x1b[0m`);
 });
