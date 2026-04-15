7475d2ac66a88081d7c559fc62fb6f97a50eec60
emiliooavila <avilavaladezjorgeemilio1010@gmail.com>
2026-04-15 11:36:07 -0600

    feat: estilos principales v1 y header.

--- frontend-libreria/src/app/app.ts
+++ frontend-libreria/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
