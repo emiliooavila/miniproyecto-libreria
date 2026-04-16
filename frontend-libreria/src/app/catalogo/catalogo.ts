65c389776e107c76981759cb8c46d21d24bc97c1
emiliooavila <avilavaladezjorgeemilio1010@gmail.com>
2026-04-15 18:12:41 -0600

    fix: ya funcionan los filtros de libros.

--- frontend-libreria/src/app/catalogo/catalogo.ts
+++ frontend-libreria/src/app/catalogo/catalogo.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit, ChangeDetectionStrategy, NgZone, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { ProductosService } from '../services/productos';
 
 @Component({
   selector: 'app-catalogo',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './catalogo.html',
   styleUrls: ['./catalogo.css'] 
 })
 export class CatalogoComponent implements OnInit {
   libros: any[] = [];
+  categoriaSeleccionada: string = '';
+  precioMaximo: number = 2000;
 
   constructor(
     private productosService: ProductosService,
@@ -32,6 +35,14 @@ export class CatalogoComponent implements OnInit {
       error: (error) => {
         console.error('Error al cargar la API:', error);
       }
+    });
+  }
+  get librosFiltrados() {
+    return this.libros.filter(libro => {
+      const coincideCategoria = this.categoriaSeleccionada === '' || libro.categoria === this.categoriaSeleccionada;
+      const coincidePrecio = libro.precio <= this.precioMaximo;
+      
+      return coincideCategoria && coincidePrecio;
     });
   }
 }
