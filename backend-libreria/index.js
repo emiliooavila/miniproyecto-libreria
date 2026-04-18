c09f78b1fb2eb0dc70ffbf6d268c248add5dbb06
emiliooavila <avilavaladezjorgeemilio1010@gmail.com>
2026-04-17 18:13:11 -0600

    prueba render.

--- backend-libreria/index.js
+++ backend-libreria/index.js
@@ -14,7 +14,8 @@ const db = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'libreria_db'
+    database: process.env.DB_NAME || 'libreria_db',
+    port: process.env.DB_PORT || 3306
 });
 
 app.get('/', (req, res) => {
